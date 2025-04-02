c3185a86a9ccdb5c5fcae5aac0564b928bdf6baf
Valentin fuenzalida <valentin.fuenzalidaribbing@elev.ga.ntig.se>
2025-04-02 13:28:35 +0200

    klar med uppgift 6

---
+++ Uppgift.6/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Uppgift._6;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    double antal;
+    double summa;
+    double medelvärdet;
+
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void btnAddera(object sender, RoutedEventArgs e)
+    {
+        if (double.TryParse(tbxTal.Text, out double tal))
+        {
+
+            tbxSumma.Text = $"{tal} + {summa} = {tal + summa}";
+            summa += tal;
+
+            antal ++;
+
+            tbxMedelvärdet.Text = $"{summa} / {antal} = {summa/antal}";
+
+        }
+    }
+}
