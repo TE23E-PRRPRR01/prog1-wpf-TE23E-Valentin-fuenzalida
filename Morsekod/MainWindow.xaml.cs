001ca0d7e6f0658ce313d546912d9d888cc540c2
Valentin fuenzalida <valentin.fuenzalidaribbing@elev.ga.ntig.se>
2025-04-06 16:15:53 +0200

    Klar med uppgift 7

---
+++ Morsekod/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Morsekod;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void btnÖversätt(object sender, RoutedEventArgs e)
+    {
+        string alfabetet = "ABCDEFGHIJKLMNOPQRSTUVWXYZÅÄÖ ";
+        string[] morse = [".-", "-...", "-.-.", "-..", ".", "..-.", "--.",
+                  "....", "..", ".---", "-.-", ".-..", "--", "-.",
+                  "---", ".--.", "--.-", ".-.", "...", "-", ".--",
+                  "...-", ".--", "-..-", "-.--", "--..", ".--.-",
+                  ".-.-", "---.", " "];
+
+        // Loopa igenom texten bokstav-för-bokstav
+        string textPåMorse = "";
+        foreach (var tecken in tbxMening.Text)
+        {
+            // Plocka ut bokstaven som söks
+            string bokstav = tecken.ToString().ToUpper();
+
+            // Hitta index i alfabetet (-1 betyder hittades inte)
+            int index = alfabetet.IndexOf(bokstav);
+            if (index != -1)
+            {
+                // Sätt samman meddelandet
+                textPåMorse += morse[index];
+            }
+        }
+        
+
+        tbxMorsekod.Text = $"{textPåMorse}";
+    }
+}
